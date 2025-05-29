eccc9de9186e2d77dcbc3b567d73f960314e4a75
Tefikujkani <149491750+Tefikujkani@users.noreply.github.com>
2025-05-29 14:41:45 +0200

    Fix TypeScript error in MongoDB connection

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -1,9 +1,18 @@
 import mongoose from 'mongoose'
 
-const MONGODB_URI = process.env.MONGODB_URI
+declare global {
+  var mongoose: {
+    conn: typeof mongoose | null
+    promise: Promise<typeof mongoose> | null
+  } | undefined
+}
+
+const MONGODB_URI = process.env.MONGODB_URI!
 
 if (!MONGODB_URI) {
-  throw new Error('Please define the MONGODB_URI environment variable inside .env.local')
+  throw new Error(
+    'Please define the MONGODB_URI environment variable inside .env.local'
+  )
 }
 
 let cached = global.mongoose
@@ -13,28 +22,28 @@ if (!cached) {
 }
 
 async function dbConnect() {
-  if (cached.conn) {
+  if (cached?.conn) {
     return cached.conn
   }
 
-  if (!cached.promise) {
+  if (!cached?.promise) {
     const opts = {
       bufferCommands: false,
     }
 
-    cached.promise = mongoose.connect(MONGODB_URI!, opts).then((mongoose) => {
+    cached!.promise = mongoose.connect(MONGODB_URI, opts).then((mongoose) => {
       return mongoose
     })
   }
 
   try {
-    cached.conn = await cached.promise
+    cached!.conn = await cached?.promise
   } catch (e) {
-    cached.promise = null
+    cached!.promise = null
     throw e
   }
 
-  return cached.conn
+  return cached?.conn
 }
 
 export default dbConnect 
